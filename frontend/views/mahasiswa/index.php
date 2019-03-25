89f2d2c5b25500c4a9cb1f9d79a2c83d31849b40
ridwansubulas <ridwansubulas@gmail.com>
2019-03-25 13:19:32 +0700

    mahasiswa

---
+++ frontend/views/mahasiswa/index.php
@@ -0,0 +1,36 @@
+<?php?>
+<h1>Daftar Mahasiswa</h1>
+<div class="container-fluid">
+	<div class="row">
+		<table class="table table-hover table-sm table-bordered">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nama Mahasiswa</th>
+					<th>Program Studi</th>
+					<th>Semester</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td> <?=$nama1?> </td>
+					<td> <?=$si?> </td>
+					<td> <?=$smt1?></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td> <?=$nama2?> </td>
+					<td> <?=$ti?> </td>
+					<td> <?=$smt2?></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td> <?=$nama3?> </td>
+					<td> <?=$ti?> </td>
+					<td> <?=$smt3  ?></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
